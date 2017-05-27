f20c495edcfe97da2d88445646415684da070ea6
nazandrey <nazandrbox@yandex.ru>
2017-05-27 11:41:31 +0300

    #3 Deleted not that useful comments

--- Assets/Scripts/SpawnRectangleOnClick.cs
+++ Assets/Scripts/SpawnRectangleOnClick.cs
@@ -3,20 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnRectangleOnClick : MonoBehaviour {
-	//variable to take rectangle prefab
 	public GameObject rectanglePrefab;
 
-	//bind spawn prefab
 	public void OnMouseDown()
 	{
-		//checking rectangle prefab existence
 		if (rectanglePrefab != null) {
-			//if found convert mouse position to main camera area
+			//convert mouse position to main camera area
 			Vector3 rectangleSpawnPosition = Input.mousePosition;
 			rectangleSpawnPosition.z = 10;
 			Instantiate (rectanglePrefab, Camera.main.ScreenToWorldPoint(rectangleSpawnPosition), new Quaternion ());
 		} else {
-			//if not found warning
 			Debug.LogWarning ("no rectangle prefab for SpawnRectangleOnClick");
 		}
 	}
