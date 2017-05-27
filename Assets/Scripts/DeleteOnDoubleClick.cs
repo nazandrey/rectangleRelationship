345b2733a4381cf07f2e4a9e4ca09079f5ea35f3
nazandrey <nazandrbox@yandex.ru>
2017-05-27 11:59:03 +0300

    Close #4 Deleting rectangle on double click

---
+++ Assets/Scripts/DeleteOnDoubleClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteOnDoubleClick : MonoBehaviour {
+	private const float Delay = 0.3F;
+
+	private bool firstClick = false;
+	private float runningTimerSecond;
+
+	public void OnMouseDown()
+	{
+		// If the time is too long we reset first click variable
+		if (firstClick && (Time.time - runningTimerSecond) > Delay) {
+			firstClick = false;
+		}
+
+		if (!firstClick) {
+			firstClick = true;
+			runningTimerSecond = Time.time;
+		} else {
+			//double click has happen
+			firstClick = false;
+			Destroy (gameObject);
+		}
+	}
+}
