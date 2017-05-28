5b50bc0ed55a6f336af1627f6992515470eb682b
nazandrey <nazandrbox@yandex.ru>
2017-05-28 15:38:13 +0300

    #3 Added constant for camera z position

--- Assets/Scripts/SpawnRectangleOnClick.cs
+++ Assets/Scripts/SpawnRectangleOnClick.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class SpawnRectangleOnClick : MonoBehaviour {
 	public GameObject rectanglePrefab;
+	private const int CameraZPosition = 10;
 
 	public void OnMouseDown()
 	{
 		if (rectanglePrefab != null) {
 			//convert mouse position to main camera area
 			Vector3 rectangleSpawnPosition = Input.mousePosition;
-			rectangleSpawnPosition.z = 10;
+			rectangleSpawnPosition.z = CameraZPosition;
 			Instantiate (rectanglePrefab, Camera.main.ScreenToWorldPoint(rectangleSpawnPosition), new Quaternion ());
 		} else {
 			Debug.LogWarning ("no rectangle prefab for SpawnRectangleOnClick");
