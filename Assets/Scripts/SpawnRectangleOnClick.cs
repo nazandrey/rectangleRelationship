3dd4efa545144cdf1057b4f3c48230816e1c3e7f
nazandrey <nazandrbox@yandex.ru>
2017-05-28 17:30:18 +0300

    Close #15 Avoiding overlapping on create

--- Assets/Scripts/SpawnRectangleOnClick.cs
+++ Assets/Scripts/SpawnRectangleOnClick.cs
@@ -3,18 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnRectangleOnClick : MonoBehaviour {
-	public GameObject rectanglePrefab;
 	private const int CameraZPosition = 10;
+	private const int RectangleLayerMask = 256; //2^8, where 8 - RectangleLayerNum
+
+	public GameObject rectanglePrefab;
 
 	public void OnMouseDown()
 	{
-		if (rectanglePrefab != null) {
-			//convert mouse position to main camera area
-			Vector3 rectangleSpawnPosition = Input.mousePosition;
-			rectangleSpawnPosition.z = CameraZPosition;
-			Instantiate (rectanglePrefab, Camera.main.ScreenToWorldPoint(rectangleSpawnPosition), new Quaternion ());
-		} else {
+		if (rectanglePrefab != null && NoRectanglesAround()) {
+			Instantiate (rectanglePrefab, getCameraMousePosition(), new Quaternion ());
+		} else if (rectanglePrefab == null) {
 			Debug.LogWarning ("no rectangle prefab for SpawnRectangleOnClick");
 		}
+	}
+
+	public bool NoRectanglesAround()
+	{
+		SpriteRenderer rectanglePrefabRenderer = rectanglePrefab.GetComponent<SpriteRenderer>();
+		Vector3 size = Vector3.zero;
+		if(rectanglePrefabRenderer != null){
+			size = rectanglePrefabRenderer.bounds.size;
+		}
+		return size != Vector3.zero ? Physics2D.OverlapBox (getCameraMousePosition (), size, 0, RectangleLayerMask) == null : false;
+	}
+
+	private Vector3 getCameraMousePosition(){
+		//convert mouse position to main camera area
+		Vector3 vector3MousePosition = Input.mousePosition;
+		vector3MousePosition.z = CameraZPosition;
+		return Camera.main.ScreenToWorldPoint(vector3MousePosition);
 	}
 }
