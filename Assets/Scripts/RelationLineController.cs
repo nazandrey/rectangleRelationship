baf3d7daf4326107383f290d4fd53430c6ec7a4e
nazandrey <nazandrbox@yandex.ru>
2017-05-30 22:45:39 +0300

    Close #17 Created relation line prefab

---
+++ Assets/Scripts/RelationLineController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelationLineController : MonoBehaviour {
+	private LineRenderer _relationLineRenderer;
+
+	public void InitStartPoint(Vector3 position){
+		_relationLineRenderer = gameObject.GetComponentInChildren<LineRenderer> ();
+		_relationLineRenderer.enabled = false;
+		_relationLineRenderer.SetPosition (0, position);
+	}
+
+	public void InitEndPoint(Vector3 position){
+		_relationLineRenderer.SetPosition (1, position);
+		_relationLineRenderer.enabled = true;
+	}
+}
