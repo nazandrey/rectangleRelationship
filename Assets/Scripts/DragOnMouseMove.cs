3a6b8a9fe470a9f551af787e728ce77cadce24b2
nazandrey <nazandrbox@yandex.ru>
2017-05-30 22:41:26 +0300

    #5, #8 Restrict drag on relation point

--- Assets/Scripts/DragOnMouseMove.cs
+++ Assets/Scripts/DragOnMouseMove.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class DragOnMouseMove : MonoBehaviour {
 	private Vector3 screenPoint;
 	private Vector3 offset;
+	private bool canDrag = true;
 
 	void OnMouseDown()
 	{
-		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+		RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero);
+
+		if (hit.collider == null || hit.collider.tag != "RelationPoint") {
+			canDrag = true;
+			screenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
+			offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+		} else {
+			canDrag = false;
+		}
+
 	}
 
 	void OnMouseDrag()
 	{
-		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-		transform.position = curPosition;
+		if (canDrag) {
+			Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+			Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint) + offset;
+			transform.position = curPosition;
+		}
 	}
 }
