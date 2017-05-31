0c1952be522afe8e7064760d40afa7149ec140de
nazandrey <nazandrbox@yandex.ru>
2017-05-31 23:18:21 +0300

    #5 Renamed private variables on drag script

--- Assets/Scripts/DragOnMouseMove.cs
+++ Assets/Scripts/DragOnMouseMove.cs
@@ -3,28 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DragOnMouseMove : MonoBehaviour {
-	private Vector3 screenPoint;
-	private Vector3 offset;
-	private bool canDrag = true;
+	private Vector3 _screenPoint;
+	private Vector3 _offset;
+	private bool _canDrag = true;
 
 	public void OnMouseDown()
 	{
 		RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero);
 
 		if (hit.collider != null && hit.collider.tag == "Rectangle") {
-			canDrag = true;
-			screenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
-			offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+			_canDrag = true;
+			_screenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
+			_offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z));
 		} else {
-			canDrag = false;
+			_canDrag = false;
 		}
 	}
 
 	public void OnMouseDrag()
 	{
-		if (canDrag) {
-			Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-			Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint) + offset;
+		if (_canDrag) {
+			Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z);
+			Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint) + _offset;
 			transform.position = curPosition;
 		}
 	}
