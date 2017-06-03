99c19ab115eaa634316d5295c8b97c73aea4553d
nazandrey <nazandrbox@yandex.ru>
2017-06-03 22:17:35 +0300

    Fix #25 Quick click after relationship doesnt delete rectangle

--- Assets/Scripts/DeleteOnDoubleClick.cs
+++ Assets/Scripts/DeleteOnDoubleClick.cs
@@ -12,21 +12,25 @@ public class DeleteOnDoubleClick : MonoBehaviour {
 
 	private void OnMouseDown()
 	{
-		// If the time is too long we reset first click variable
-		if (_firstClick && (Time.time - _runningTimerSecond) > _Delay) {
-			_firstClick = false;
-		}
+		RaycastHit2D hit = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), Vector2.zero);
+		//handle only if we click exactly on visible part of this gameobject
+		if (hit.collider != null && hit.collider.gameObject == gameObject) {
+			// If the time is too long we reset first click variable
+			if (_firstClick && (Time.time - _runningTimerSecond) > _Delay) {
+				_firstClick = false;
+			}
 
-		if (!_firstClick) {
-			_firstClick = true;
-			_runningTimerSecond = Time.time;
-		} else {
-			//double click has happen
-			_firstClick = false;
-			if (destroyParent) {
-				Destroy (transform.parent.gameObject);
-			} else {	
-				Destroy (gameObject);
+			if (!_firstClick) {
+				_firstClick = true;
+				_runningTimerSecond = Time.time;
+			} else {
+				//double click has happen
+				_firstClick = false;
+				if (destroyParent) {
+					Destroy (transform.parent.gameObject);
+				} else {
+					Destroy (gameObject);
+				}
 			}
 		}
 	}
